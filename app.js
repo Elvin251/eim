4a18f342976de6651cc9607111c89ae564231555
Elvin251 <emmmdov485@gmail.com>
2024-12-15 21:24:57 +0400

    d

---
+++ app.js
@@ -0,0 +1,7 @@
+let a= 0;
+
+function nor() {
+a=document.getElementById("inertex").value;
+document.getElementById("tall").innerText=("UpperCase :" + a.toLocaleUpperCase())
+document.getElementById("short").innerText=("LowerCase :" + a.toLocaleLowerCase())
+}
